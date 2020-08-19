5b1e8937403dfd881c6db512ea7383ecc1817409
tomlenbzh <thomas.lenoel@epitech.eu>
2020-08-19 15:53:33 +0200

    ADD PAGE_NOT_FOUND MODULE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
+  }, {
+    path: 'page-not-found',
+    loadChildren: () => import('./pages/page-not-found/page-not-found.module').then(m => m.PageNotFoundModule)
+  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'page-not-found' }
 ];
 
 @NgModule({
--- src/app/pages/page-not-found/page-not-found-routing.module.ts
+++ src/app/pages/page-not-found/page-not-found-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login.component';
+import { PageNotFoundComponent } from './page-not-found.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', component: PageNotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class LoginRoutingModule { }
+export class PageNotFoundRoutingModule { }
